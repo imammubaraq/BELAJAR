70a66d144efd55226bba3681bf385207a3570ea0
imammubaraq <imammubaraq12@gmail.com>
2022-02-20 03:08:44 -0800

    update

---
+++ mencoba.php
@@ -0,0 +1,25 @@
+<html>
+   
+   <head>
+      <title>belajar</title>
+   </head>
+   
+   <body>
+      
+      <?php
+      $color = ['red','green','blue'];
+      foreach ($colors as $color) {
+        echo $color . '<br>';
+    }if ($a > $b) {
+            echo "red";
+        } elseif ($a == $b) {
+            echo "blue";
+        } else {
+            echo "green";
+        }
+        $colors = ['red', 'green', 'blue'];
+      ?>
+      
+   </body>
+</html>
+?>
