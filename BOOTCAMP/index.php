f5b2424383bc6b65d3685f6385d9ca754a8b3624
imammubaraq <imammubaraq12@gmail.com>
2022-02-20 03:21:07 -0800

    update

---
+++ BOOTCAMP/index.php
@@ -0,0 +1,20 @@
+<html>
+   
+   <head>
+      <title>Function</title>
+   </head>
+   
+   <body>
+      
+      <?php
+         $foodArray = ["Eggs", "Bacon", "HashBrowns", "Beans", "Bread"];
+
+
+         foreach ($foodArray as $food)  {
+             echo $food ."<br />";
+         }
+      ?>
+      
+   </body>
+</html>
+?>
